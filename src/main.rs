e0532beb8f29102da12721079292dee6ae2176cd
Mark Thornber <mark.thornber@gmail.com>
2023-08-03 15:11:44 +0100

    Move validation of configuration into separate module.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_files as fs;
 use actix_web::{web, App, HttpServer};
 use dotenv::dotenv;
 use std::collections::HashMap;
-use std::env;
+use std::process;
 use std::sync::Mutex;
 use tera::{from_value, to_value, Function, Tera, Value};
 
@@ -11,10 +11,12 @@ mod handlers;
 mod models;
 mod routes;
 mod state;
+mod validation;
 
 use canpi_config::*;
 use routes::*;
 use state::AppState;
+use validation::*;
 
 fn make_scope_for<'a>(scopes: &'static HashMap<&'a str, String>) -> impl Function + 'a {
     Box::new(
@@ -33,54 +35,52 @@ fn make_scope_for<'a>(scopes: &'static HashMap<&'a str, String>) -> impl Functio
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
+    if let Ok(canpi_cfg) = CanpiConfig::new() {
+        // Configuration file names
+        let autohs_file = canpi_cfg.autohs_ini_path.unwrap();
+        let canpi_file = canpi_cfg.canpi_ini_path.unwrap();
+        // WEbpage formatting files
+        let static_path = canpi_cfg.static_path.unwrap();
+        // Create and load the autohs configuration using the JSON schema file
+        let mut cfg_autohs = Cfg::new();
+        cfg_autohs
+            .load_configuration(autohs_file.clone(), canpi_cfg.config_path.clone().unwrap())
+            .expect("Cannot load autohs configuration");
 
-    // Define JSON schema file
-    let def_file =
-        concat!(env!("CARGO_MANIFEST_DIR"), "/static/canpi-config-defn.json").to_string();
+        // Create and load the canpi configuration using the JSON schema file
+        let mut cfg_canpi = Cfg::new();
+        cfg_canpi
+            .load_configuration(canpi_file.clone(), canpi_cfg.config_path.clone().unwrap())
+            .expect("Cannot load canpi configuration");
 
-    // Create and load the autohs configuration
-    let autohs_ini_file = env::var("AUTOHS_INI_FILE")
-        .expect("AUTOHS_INI_FILE path is not set in .env file")
-        .to_string();
-    let mut cfg_autohs = Cfg::new();
-    cfg_autohs
-        .load_configuration(autohs_ini_file.clone(), def_file.clone())
-        .expect("Cannot load autohs configuration");
-
-    // Create and load the canpi configuration
-    let canpi_ini_file = env::var("CANPI_INI_FILE")
-        .expect("CANPI_INI_FILE path is not set in .env file")
-        .to_string();
-    let mut cfg_canpi = Cfg::new();
-    cfg_canpi
-        .load_configuration(canpi_ini_file.clone(), def_file.clone())
-        .expect("Cannot load canpi configuration");
-
-    // Start HTTP Server
-    let host_port = env::var("HOST_PORT").expect("HOST_PORT address is not set in .env file");
-    let shared_date = web::Data::new(Mutex::new(AppState {
-        layout_name: hostname::get()?.into_string().unwrap(),
-        project_id: "{project_id}".to_string(),
-        autohs_ini_file: autohs_ini_file.clone(),
-        canpi_ini_file: canpi_ini_file.clone(),
-        autohs_cfg: cfg_autohs,
-        canpi_cfg: cfg_canpi,
-    }));
-    let mut tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")).unwrap();
-    tera.register_function("scope_for", make_scope_for(&ROUTE_DATA));
-    let app = move || {
-        App::new()
-            .data(tera.clone())
-            .app_data(shared_date.clone())
-            .configure(autohs_routes)
-            .configure(canpi_routes)
-            .configure(general_routes)
-            .service(fs::Files::new("/static", "./canpi-web-app-ssr/static").show_files_listing())
-    };
-
-    println!(
-        "Listening on: {}, open browser and visit - have a go!",
-        host_port
-    );
-    HttpServer::new(app).bind(&host_port)?.run().await
+        // Start HTTP Server
+        let host_port = canpi_cfg.host_port.unwrap();
+        let shared_date = web::Data::new(Mutex::new(AppState {
+            layout_name: hostname::get()?.into_string().unwrap(),
+            project_id: "{project_id}".to_string(),
+            autohs_ini_file: autohs_file.clone(),
+            canpi_ini_file: canpi_file.clone(),
+            autohs_cfg: cfg_autohs,
+            canpi_cfg: cfg_canpi,
+        }));
+        let mut tera = Tera::new(canpi_cfg.template_path.unwrap().as_str()).unwrap();
+        tera.register_function("scope_for", make_scope_for(&ROUTE_DATA));
+        let app = move || {
+            App::new()
+                .data(tera.clone())
+                .app_data(shared_date.clone())
+                .configure(autohs_routes)
+                .configure(canpi_routes)
+                .configure(general_routes)
+                .service(fs::Files::new("/static", static_path.clone()).show_files_listing())
+        };
+        println!(
+            "Listening on: {}, open browser and visit - have a go!",
+            host_port
+        );
+        HttpServer::new(app).bind(&host_port)?.run().await
+    } else {
+        println!("EV contents failed validation - exiting ...");
+        process::exit(1);
+    }
 }
---
+++ src/validation.rs
@@ -0,0 +1,108 @@
+use crate::errors::CanPiAppError;
+use std::borrow::ToOwned;
+use std::path::Path;
+#[macro_export]
+macro_rules! pkg_name {
+    () => {
+        env!("CARGO_PKG_NAME")
+    };
+}
+
+const CANPI_SSR_DIR: &str = pkg_name!();
+//const CANPI_SSR_DIR: &str = env!("CARGO_PKG_NAME");
+const CFGFILE: &str = "/canpi-config-defn.json";
+const STATIC: &str = "/static";
+const TEMPLATE: &str = "/templates/**/*";
+
+#[derive(Clone)]
+/// Structure that holds configuration items expanded from EVs and static text
+pub struct CanpiConfig {
+    pub autohs_ini_path: Option<String>,
+    pub canpi_ini_path: Option<String>,
+    pub config_path: Option<String>,
+    pub host_port: Option<String>,
+    pub static_path: Option<String>,
+    pub template_path: Option<String>,
+}
+
+impl CanpiConfig {
+    /// Creates a new instance  of the structure
+    ///
+    /// The contents of the EV CPS_HOME is used with the static text above to create path strings
+    /// for items.
+    ///
+    /// If CPS_HOME is not defined or does not point to a valid directory then an error result is
+    /// returned.
+    ///
+    /// If the EVs AUTOHS_INI_FILE or CANPI_INI_FILE are not defined or do not point to valid files
+    /// then the entries in the struct are set to None.
+    ///
+    /// If the EV HOST_PORT is not defined then the entry in the struct is set to None.  No further
+    /// validation is done if the EV does exist.
+    ///
+    pub fn new() -> Result<CanpiConfig, CanPiAppError> {
+        let h = std::env::var("CPS_HOME");
+        match h {
+            Ok(home) => {
+                let cps_home = home;
+                if !Path::new(&cps_home).is_dir() {
+                    return Err(CanPiAppError::NotFound(
+                        "EV CPS_HOME not a directory".to_string(),
+                    ));
+                }
+                let mut cfg = CanpiConfig {
+                    autohs_ini_path: None,
+                    canpi_ini_path: None,
+                    config_path: None,
+                    host_port: None,
+                    static_path: None,
+                    template_path: None,
+                };
+                if let Ok(autohs) = std::env::var("AUTOHS_INI_FILE") {
+                    if Path::new(&autohs).is_file() {
+                        cfg.autohs_ini_path = Some(autohs);
+                    } else {
+                        return Err(CanPiAppError::NotFound(
+                            "EV AUTOHS_INI_FILE not valid".to_string(),
+                        ));
+                    }
+                }
+                if let Ok(canpi) = std::env::var("CANPI_INI_FILE") {
+                    if Path::new(&canpi).is_file() {
+                        cfg.canpi_ini_path = Some(canpi);
+                    } else {
+                        return Err(CanPiAppError::NotFound(
+                            "EV CANPI_INI_FILE not valid".to_string(),
+                        ));
+                    }
+                }
+                let cfile = cps_home.clone() + "/" + CANPI_SSR_DIR + STATIC + CFGFILE;
+                if Path::new(&cfile).is_file() {
+                    cfg.config_path = Some(cfile);
+                }
+
+                if let Ok(port) = std::env::var("HOST_PORT") {
+                    cfg.host_port = Some(port);
+                } else {
+                    return Err(CanPiAppError::NotFound(
+                        "EV HOST_PORT not valid".to_string(),
+                    ));
+                }
+                let sdir = cps_home.clone() + "/" + CANPI_SSR_DIR + STATIC;
+                if Path::new(&sdir).is_dir() {
+                    cfg.static_path = Some(sdir);
+                }
+                let tdir = cps_home.clone() + "/" + CANPI_SSR_DIR + TEMPLATE;
+                let grandparent = Path::new(&tdir).parent().unwrap().parent().unwrap();
+                if grandparent.is_dir() {
+                    cfg.template_path = Some(tdir);
+                }
+                return Ok(cfg);
+            }
+            _ => {}
+        }
+        Err(CanPiAppError::NotFound(
+            "EV CPS_HOME not defined".to_string(),
+        ))
+    }
+}
